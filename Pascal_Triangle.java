7034e8209c347900f8d6961652fb58b7df50d4d6
Uttkarsh <138629133+oreki07@users.noreply.github.com>
2026-03-17 20:15:55 +0530

    fixed pascal triangle code

--- Pascal_Triangle.java
+++ Pascal_Triangle.java
@@ -7,6 +7,11 @@ In Pascal's triangle, each number is the sum of the two numbers directly above i
  
 
 Example 1:
+        1
+       1 1
+      1 2 1
+     1 3 3 1
+    1 4 6 4 1 
 
 Input: numRows = 5
 Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
@@ -27,21 +32,21 @@ Constraints:
 import java.util.*;
 public class Pascal_Triangle {
     public List<List<Integer>> generate (int numRows){
-        int[][] dp = new int [numRows][numRows];
+        int[][] dp = new int [numRows][numRows]; // dp array creation 
 
-        List<List<Integer>> result = new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>(); // creating a result list 
 
-        for(int i = 0; i <= numRows ; i++){
+        for(int i = 0; i <= numRows ; i++){ // outer loop (rows)
 
-            List<Integer> row = new ArrayList<>();
+            List<Integer> row = new ArrayList<>(); // create new list row 
 
-            for(int j = 0 ; i <= i ; i++){
-                if(j == 0 || j == i ){
+            for(int j = 0 ; i <= i ; i++){ // inner loop for (columns)
+                if(j == 0 || j == i ){ // the first and last element will always be 1
                     dp[i][j] = 1;
                 }else{
-                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j]; // basically take an example of dp [3][1] = dp[i-1 = 2][j-1 = 0] + dp[2][1] so the answer will be 2 + 1 = 3
                 }
-                row.add(dp[i][j]);
+                row.add(dp[i][j]); 
             }
             result.add(row);
 
